fafacde90b584ac03fa6f827204f3b04bf39ee75
avijeet <avijeetjana26@gmail.com>
2024-12-13 03:06:12 +0530

    adding test file

---
+++ src/DecPractice/Prime02.java
@@ -0,0 +1,39 @@
+package src.DecPractice;
+
+import java.util.Scanner;
+
+public class Prime02 {
+    public static void main(String[] args) {
+        boolean prime = true;
+        Scanner sc =  new Scanner(System.in);
+        System.out.println("Provide a Number");
+        int num = sc.nextInt();
+
+        if (num<0)
+        {
+            System.out.println("Number is less than Zero. Please provide another Number");
+        } else if (num==0)
+        {
+            System.out.println("Number is Zero. Please Provide another Number");
+        } else if (num>0)
+        {
+            for (int i =2;i<num;i++)
+            {
+                if (num%i==0)
+                {
+                    prime = false;
+                    break;
+                }
+            }
+
+            if (prime)
+            {
+                System.out.println("Prime Number");
+            }
+            else
+            {
+                System.out.println("Not Prime Number");
+            }
+        }
+    }
+}
